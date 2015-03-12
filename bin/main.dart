9d14958dbea9e3b125fe8318d0e0effa1f6b3bdd
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2015-03-12 14:52:13 +0100

    simplify launch

--- bin/main.dart
+++ bin/main.dart
@@ -3,16 +3,10 @@
 
 import 'dart:io';
 
-import 'package:args/args.dart';
 import 'package:hashcode2015/hashcode2015.dart' as hashcode2015;
 
 main(List<String> args) {
-  final parser = new ArgParser()..addOption('file', abbr: 'f');
-
-  final result = parser.parse(args);
-  final file = result['file'];
-
-  final input = new File(file).readAsStringSync();
+  final input = new File(args[0]).readAsStringSync();
 
   print(hashcode2015.compute(input));
 }
