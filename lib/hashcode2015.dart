1fe9487ac543cb2015db73a409e71ba43f441add
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2015-03-12 19:30:24 +0100

    model

--- lib/hashcode2015.dart
+++ lib/hashcode2015.dart
@@ -7,3 +7,35 @@ library hashcode2015;
 String compute(String input) {
   return input;
 }
+
+class Machine {
+  final int id, slots, capacity;
+
+  Machine(this.id, this.slots, this.capacity);
+}
+
+final UNAVAILABLE = new Machine(null, null, null);
+
+class Group {
+  final int id;
+
+  Group(this.id);
+}
+
+class DataCenter {
+  final List<List<Machine>> slots;
+
+  DataCenter(int rows, int rowSize)
+      : slots = new List.generate(rows, (_) => new List<Machine>(rowSize));
+
+  void put(int row, int slot, Machine m) {
+    slots[row][slot] = m;
+  }
+}
+
+class Installation {
+  Machine machine;
+  Group group;
+  int row, slot;
+  Installation(this.machine, this.group, this.row, this.slot);
+}
