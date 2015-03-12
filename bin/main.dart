6de0826ac1a3ef6e49df4455f2e22d4bd7949afa
William Delanoue <william.delanoue@deveryware.net>
2015-03-12 20:55:57 +0100

    show

--- bin/main.dart
+++ bin/main.dart
@@ -21,7 +21,6 @@ main(List<String> args) {
       firstLine[0], firstLine[1], firstLine[2], firstLine[3], firstLine[4]);
 
   final dataCenter = new DataCenter(info.rows, info.rowSize);
-
   allLines
       .getRange(1, info.indispoSize + 1)
       .map((f) => f.split(" ").map((p) => int.parse(p)).toList())
@@ -34,8 +33,21 @@ main(List<String> args) {
       .map((f) => new Machine(i++, f[0], f[1]))
       .toList();
 
-  print(dataCenter);
-  machines.forEach((m) => print(m));
+//  print(dataCenter);
+//  machines.forEach((m) => print(m));
 
+  final optim = optimize1(dataCenter, machines,
+          new Iterable.generate(info.groups).map((i) => new Group(i)).toList())
+      .where((i) => i.machine != UNAVAILABLE).toList();
+
+  optim.sort((i1, i2) => i1.machine.id - i2.machine.id);
+  final result = optim.map((i) => "${i.row} ${i.slot} ${i.group.id}").toList();
+
+  final allIdMachinesInOptim = optim.map((i) => i.machine.id).toList();
+
+  final unavailables =
+      machines.where((m) => !allIdMachinesInOptim.contains(m.id));
+  result.forEach((r) => print(r));
+  unavailables.forEach((r) => print("x"));
 //  print(hashcode2015.compute(input));
 }
